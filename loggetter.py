3a09115706c6c110403c0d69a77eb74377d1edbb
bigreybear <x_zhao16@163.com>
2018-06-07 16:23:12 +0800

    a

--- loggetter.py
+++ loggetter.py
@@ -5,7 +5,8 @@ import os
 
 # Change dir silently to load the config file.
 ori_path = os.getcwd()
-os.chdir("/Pyproject/ProfGetter/")
+# os.chdir("/Pyproject/ProfGetter/")
+os.chdir("D:\GitRepo\progetter\\")
 print("Process on:" + str(os.getcwd()))
 with open("log.yaml") as f:
     log_ = logging.config.dictConfig(yaml.load(f))
