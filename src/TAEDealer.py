0cc0856ab64714c2d9eb516a1c7ddf9c7262fc43
bigreybear <x_zhao16@163.com>
2018-07-20 10:37:48 +0800

    should be a perfect tae

--- src/TAEDealer.py
+++ src/TAEDealer.py
@@ -1,6 +1,7 @@
 from AbsDealer import AbsDealer as AD
 import lxml.etree as etree
-import urllib, urllib2
+import time
+from loggetter import logger
 
 class TAEDealer(AD):
     def __init__(self):
@@ -12,18 +13,16 @@ class TAEDealer(AD):
         self.header['Accept-Language'] = "zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,es;q=0.6,zh-TW;q=0.5"
         self.header['Content-Type'] = "application/json"
 
-        # self.xpath_dic['href'] = '//div[@class="dir-member-name"]/a/@href'
-        # self.xpath_dic['name'] = '//div[@class="dir-member-details"]/h3'
-        # self.xpath_dic['year_selected'] = '//div[@class="member-info-section"]/div'
-        # self.xpath_dic['citation'] = '//div[@class="member-details-text"]/p'
-        # self.xpath_dic['previous_service'] = '//div[@class="member-info-section"]/ul/li'
-        # self.xpath_dic['bio'] = '//div[@class="expandableBio"]/p'
-
         self.xpath_dic['Present and Previous Positions'] = '//b[text()="Present and Previous Positions"]'
         self.xpath_dic['Fields of Scholarship'] = '//b[text()="Fields of Scholarship"]'
         self.xpath_dic['Honours and Awards'] = '//b[text()="Honours and Awards"]'
 
-        self.xpath_dic['Email'] = '//td[text()="Email:"]/following-sibling::td'
+        self.xpath_dic['Email'] = '//td[text()="Email:"]'
+        self.xpath_dic['Membership type'] = '//td[text()="Membership type:"]'
+        self.xpath_dic['Main Country of Residence'] = '//td[text()="Main Country of Residence:"]'
+        self.xpath_dic['Section'] = '//td[text()="Section:"]'
+        self.xpath_dic['Elected'] = '//td[text()="Elected:"]'
+        self.xpath_dic['name'] = '//div[@id="pagecontent"]'
 
         self.url_xpath = '//span[@class="name"]/b/a/@href'
         self.next_button = '//span[@class="last"]/../@href'
@@ -35,6 +34,13 @@ class TAEDealer(AD):
             "Present and Previous Positions"
         ]
 
+        self.target_list_2 = [
+            "Email",
+            "Membership type",
+            "Main Country of Residence",
+            "Elected"
+        ]
+
         self.tmp_save_name = "tae.tmp"
         self.fin_save_name = "tae.bin"
         self.person_page_root = ""
@@ -44,8 +50,6 @@ class TAEDealer(AD):
         self.max_retry = 8
 
     def collect_urls(self, _start_url=None):
-        # _content = self.pros_page(_start_url)
-        # _content = urllib2.urlopen(_start_url).read()
         _content = self.simple_get(_start_url, True)
         while _content is not None:
             _tree = etree.HTML(_content)
@@ -58,7 +62,6 @@ class TAEDealer(AD):
                 break
             nxt_els = _tree.xpath(self.next_button)
             if len(nxt_els) > 1:
-                # _content = self.pros_page(nxt_els[0])
                 _content = self.simple_get(nxt_els[0], True)
         self.dealer_dump(True, False, True)
 
@@ -67,14 +70,40 @@ class TAEDealer(AD):
 
         _this_prf = {}
         for target in self.target_list:
-            if _content.xpath(self.xpath_dic[target])[0] is not None:
-                _this_prf[target] = "; ".join([x.text[:-1] for x in _content.xpath(self.xpath_dic[target])[0].getnext().getchildren()])
+            if len(_content.xpath(self.xpath_dic[target])) > 0 and \
+                    _content.xpath(self.xpath_dic[target])[0] is not None:
+                try:
+                    _this_prf[target] = "; ".join([x.text[:-1] for x in _content.xpath(self.xpath_dic[target])[0].getnext().getchildren()])
+                except TypeError as e:
+                    _this_prf[target] = ""
+                    logger.error("An error occurred.\n{}".format(e.message))
 
-        print(_content.xpath(self.xpath_dic['Email']).text)  # CANNOT FIND CORRECT ELEMENT
+        for target in self.target_list_2:
+            if len(_content.xpath(self.xpath_dic[target])) > 0 and \
+                    _content.xpath(self.xpath_dic[target])[0] is not None:
+                try:
+                    _this_prf[target] = _content.xpath(self.xpath_dic[target])[0].getnext().text
+                except TypeError as e:
+                    _this_prf[target] = ""
+                    logger.error("An error occurred.\n{}".format(e.message))
+
+        _this_prf['name'] = _content.xpath(self.xpath_dic['name'])[0].getchildren()[0].text
+        _this_prf['Section'] = _content.xpath(self.xpath_dic['Section'])[0].getnext().getchildren()[0].text
+
         self.dict_printer(_this_prf)
+        return _this_prf
+
+    def collect_personal_info(self):
+        for url in self.url_list:
+            time.sleep(0.5)
+            self.prf_list.append(self.personal_info(url))
+            self.dealer_dump(False, True, False)
+            print("{}/{} professors had been dumped.".format(len(self.prf_list), len(self.url_list)))
+        self.dealer_dump(False, True, True)
 
 if __name__ == '__main__':
     tae = TAEDealer()
-    # print(tae.simple_get(tae.test_url))
     # tae.collect_urls(tae.test_url)
-    tae.personal_info("https://www.ae-info.org/ae/User/Zwirner_Fabio")
+    tae.rebuild("url_tae.bin")
+    tae.url_list = list(set(tae.url_list))
+    tae.collect_personal_info()
